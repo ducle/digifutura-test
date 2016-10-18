6acbed98122d7cbdbc3a4d6d78f0353f11ca5f8c
Ngoc <ngoc@digifutura.com>
2016-10-18 10:44:39 +0700

    added model question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018032735) do
+ActiveRecord::Schema.define(version: 20161018033604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20161018032735) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "point"
+    t.integer  "questionnaire_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["questionnaire_id"], name: "index_questions_on_questionnaire_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +48,5 @@ ActiveRecord::Schema.define(version: 20161018032735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "questions", "questionnaires"
 end
---
+++ spec/factories/questions.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :question do
+    content "which is the latest version of ruby?"
+    point 1
+    questionnaire nil
+  end
+end
