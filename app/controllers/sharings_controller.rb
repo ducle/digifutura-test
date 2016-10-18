a092e9a96d327810d9d30034eeaca76d072a2bf7
TinLe <tinle1201@gmail.com>
2016-10-18 16:53:33 +0700

    add sharings controller

--- app/controllers/sharings_controller.rb
+++ app/controllers/sharings_controller.rb
@@ -1,14 +1,15 @@
 class SharingsController < ApplicationController
 
   def index
-    @nodes = FileNode.roots.page(params[:page])
+    @nodes = current_user.accessible_file_nodes.page(params[:page])
   end
 
   def show
-    @file_node = my_nodes.find(params[:id])
+    @file_node = FileNode.find(params[:id])
+    unless current_user.can_access_node?(@file_node)
+      redirect_to sharings_path and return
+    end
     @nodes = @file_node.children.by_name.page(params[:page])
   end
-
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
   has_and_belongs_to_many :accessible_file_nodes, class_name: 'FileNode'
 
   validates :name, presence: true, length: {minimum: 2, maximum: 40}
+
+  def can_access_node?(node)
+    true
+  end
+
 end
