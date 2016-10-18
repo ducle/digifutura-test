b2930856a6354a821c568051c670bcbf34d85b66
Duc Le <ducle.sg@gmail.com>
2016-10-18 16:59:46 +0700

    validate uniqueness for user quiz

--- app/models/user_quiz.rb
+++ app/models/user_quiz.rb
@@ -1,4 +1,6 @@
 class UserQuiz < ApplicationRecord
   belongs_to :user
   belongs_to :questionnaire
+
+  validates :user_id, uniqueness: { scope: :questionnaire_id, message: "You have taken this quiz before!"}
 end
