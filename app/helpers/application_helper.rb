7766d2c23a8a66e3ba0d9eb5f3c6196701546189
TinLe <tinle1201@gmail.com>
2016-10-19 13:57:31 +0700

    add explore files page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def current_path?(path)
+    case path
+    when Regexp
+      cur_path = request.fullpath
+      cur_path.match(path) ? 'active' : ''
+    when String
+      if path =~ /#/
+        paths = path.split('#')
+        (controller_name == paths.first && action_name == paths.last) ? 'active' : ''
+      else
+        paths = controller_path.split('/')+[action_name]
+        paths.include?(path) ? 'active' : ''
+      end
+    end
+  end
+
+  
 end
